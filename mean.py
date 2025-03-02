80ebf5a32ee06671f286f70909c74b97912655c9
rezaanastasya <rezaanastasya3@gmail.com>
2025-03-02 01:14:36 -0800

    mean.py
    Contoh soal Mean

---
+++ mean.py
@@ -0,0 +1,6 @@
+# Dataset tinggi badan siswa
+tinggi_badan = [160, 165, 170, 155, 175, 180, 165, 170, 160, 165]
+
+# Menghitung Mean (Rata-rata)
+mean = sum(tinggi_badan) / len(tinggi_badan)
+print("Rata-rata tinggi badan:", mean)
